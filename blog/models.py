cde61a011b8287dee4add7c2ec8cf48e28b924b5
miran <kcrt1020@naver.com>
2022-05-26 18:10:09 +0900

    미리보기와 요약문 추가하기

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,7 @@ import os
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
+    hook_text = models.CharField(max_length=100, blank=True)
     content = models.TextField()
 
     #auther : 추후 작성 예정정
